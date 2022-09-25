2393573fd24e80d2d1be460ebce3a48ab2511120
ahmetcangurel <cangureliletisim@gmail.com>
2022-09-25 13:59:53 +0300

    Created => Empty screen

--- src/Screens/FavoriteScreen/FavoriteScreen.Style.js
+++ src/Screens/FavoriteScreen/FavoriteScreen.Style.js
@@ -8,7 +8,7 @@ export default StyleSheet.create({
         alignItems: 'center',
     },
     text: {
-        color: Colors.gray,
+        color: Colors.darkGray,
         marginTop: 16,
     }
 })
--- src/Screens/FavoriteScreen/FavoriteScreen.js
+++ src/Screens/FavoriteScreen/FavoriteScreen.js
@@ -1,13 +1,16 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text } from 'react-native'
+
 import styles from './FavoriteScreen.Style'
 import Favorite from '../../Components/icons/Favorite'
+import Colors from '../../utils/Colors'
+
 
 
 const FavoriteScreen = () => {
     return (
         <View style={styles.container}>
-            <Favorite />
+            <Favorite size={48} fill={Colors.darkGray} />
             <Text style={styles.text}>Henüz favori yok.</Text>
         </View>
     )

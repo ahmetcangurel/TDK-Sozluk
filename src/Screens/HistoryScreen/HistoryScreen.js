b00a0ea4d6235a01af94330049b71e40fe658050
ahmetcangurel <cangureliletisim@gmail.com>
2022-09-25 14:00:15 +0300

    Created => Empty screen

--- src/Screens/HistoryScreen/HistoryScreen.js
+++ src/Screens/HistoryScreen/HistoryScreen.js
@@ -3,11 +3,12 @@ import { View, Text } from 'react-native'
 import styles from './HistoryScreen.Style'
 
 import History from '../../Components/icons/History'
+import Colors from '../../utils/Colors'
 
 const DetailScreen = () => {
     return (
         <View style={styles.container}>
-            <History style={styles.icon} />
+            <History fill={Colors.darkGray} size={48} />
             <Text style={styles.text}>Henüz geçmiş yok.</Text>
         </View>
     )

137fa86169180caa3ae20a4399f4e333eaf80af8
ahmetcangurel <cangureliletisim@gmail.com>
2023-01-03 22:51:27 +0300

    Create => History data list and reducer

--- src/Redux/reducers.js
+++ src/Redux/reducers.js
@@ -3,6 +3,9 @@ const reducers = (state, action) => {
         case 'UPDATE_FAVORITE_LIST':
             return { ...state, favoriteList: action.payload }
 
+        case 'UPDATE_HISTORY_LIST':
+            return { ...state, historyList: action.payload }
+
         default:
             return state
     }
--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,5 +1,6 @@
 const state = {
     favoriteList: [],
+    historyList: [],
 }
 
 export default state

901c0a9bc1b287c0acdef6e981571e9d2c26d6ac
ahmetcangurel <cangureliletisim@gmail.com>
2023-01-03 22:53:40 +0300

    Fixed => visible states / Add => clear data func

--- src/Screens/HistoryScreen/HistoryScreen.Style.js
+++ src/Screens/HistoryScreen/HistoryScreen.Style.js
@@ -7,8 +7,22 @@ export default StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+    clearData: {
+        marginLeft: 'auto',
+        marginRight: 8,
+        padding: 6,
+    },
+    clearDataText: {
+        color: Colors.red,
+        fontSize: 12,
+    },
     text: {
         color: Colors.darkGray,
         marginTop: 16,
+    },
+    flatList: {
+        flex: 1,
+        width: '100%',
+        paddingHorizontal: 8,
     }
 })
--- src/Screens/HistoryScreen/HistoryScreen.js
+++ src/Screens/HistoryScreen/HistoryScreen.js
@@ -1,15 +1,45 @@
-import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, Text, SafeAreaView, FlatList, TouchableOpacity, Alert } from 'react-native'
 import styles from './HistoryScreen.Style'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useDispatch, useSelector } from 'react-redux'
 
+import SearchItem from '../../Components/SearchItem/SearchItem'
 import History from '../../Components/icons/History'
 import Colors from '../../utils/Colors'
+import SvgTrash from '../../Components/icons/Trash'
 
 const DetailScreen = () => {
+    const history = useSelector(s => s.historyList)
+    const dispatch = useDispatch()
+
+    const clearAllHistory = () => {
+        AsyncStorage.removeItem('@history')
+        dispatch({
+            type: 'UPDATE_HISTORY_LIST',
+            payload: []
+        })
+    }
+
     return (
         <SafeAreaView style={styles.container}>
-            <History fill={Colors.darkGray} size={48} />
-            <Text style={styles.text}>Henüz geçmiş yok.</Text>
+            {!history.length ?
+                <>
+                    <History fill={Colors.darkGray} size={48} />
+                    <Text style={styles.text}>Henüz geçmiş yok.</Text>
+                </>
+                :
+                <>
+                    <TouchableOpacity style={styles.clearData} onPress={() => clearAllHistory()}>
+                        <Text style={styles.clearDataText}>Tümünü Temizle!</Text>
+                    </TouchableOpacity>
+                    <FlatList
+                        style={styles.flatList}
+                        data={history}
+                        renderItem={({ item }) => <SearchItem title={item.name} onPress={null} />}
+                    />
+                </>
+            }
         </SafeAreaView>
     )
 }

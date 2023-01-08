7c1922b7894adeb96cc3be87435ff7ff93f3aa1b
ahmetcangurel <cangureliletisim@gmail.com>
2023-01-08 23:02:33 +0300

    Change => prop names

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Text, TouchableOpacity } from 'react-native'
 import styles from './Button.Style'
 
-const Button = ({ text, bgColor, textColor, onPress }) => {
+const Button = ({ title, bgColor, titleColor, onPress }) => {
   return (
-    <TouchableOpacity onPress={onPress} style={[styles.container, { backgroundColor: bgColor }]}>
-      <Text style={[styles.text, { color: textColor }]}>{text}</Text>
+    <TouchableOpacity onPress={onPress} style={[styles.container, { backgroundColor: bgColor, }]}>
+      <Text style={[styles.text, { color: titleColor }]}>{title}</Text>
     </TouchableOpacity>
   )
 }

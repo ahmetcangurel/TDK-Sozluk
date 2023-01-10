09b04b18f34b3a665eb9a93a94b1a816fdb7f7e5
ahmetcangurel <cangureliletisim@gmail.com>
2023-01-10 14:59:42 +0300

    Create =>favorite and history stacks

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,43 +1,68 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import BottomNav from './BottomNav';
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createStackNavigator } from '@react-navigation/stack'
+import BottomNav from './BottomNav'
 
 //Tab Screens
-import HistoryScreen from '../Screens/HistoryScreen';
-import SearchScreen from '../Screens/SearchScreen';
-import FavoriteScreen from '../Screens/FavoriteScreen';
+import HistoryScreen from '../Screens/HistoryScreen'
+import SearchScreen from '../Screens/SearchScreen'
+import FavoriteScreen from '../Screens/FavoriteScreen'
 
 //Stack Screens
-import InfoScreen from '../Screens/InfoScreen';
-import AboutScreen from '../Screens/AboutScreen';
-import ContactScreen from '../Screens/ContactScreen';
-import DetailScreen from '../Screens/DetailScreen/DetailScreen';
-import SearchDetailScreen from '../Screens/SearchDetailScreen';
+import DetailScreen from '../Screens/DetailScreen/DetailScreen'
+import SearchDetailScreen from '../Screens/SearchDetailScreen'
 
-const Tab = createBottomTabNavigator();
-const SearchStack = createStackNavigator();
+const Tab = createBottomTabNavigator()
+const Stack = createStackNavigator()
 
-function SearchStackScreen() {
+const SearchStackScreen = () => {
     return (
-        <SearchStack.Navigator
+        <Stack.Navigator
             initialRouteName='SearchScreen'
             screenOptions={{
                 headerShown: false,
             }}
         >
-            <SearchStack.Group>
-                <SearchStack.Screen name="SearchScreen" component={SearchScreen} />
-                <SearchStack.Screen name='Detail' component={DetailScreen} />
-                <SearchStack.Screen name='SearchDetail' component={SearchDetailScreen} />
-            </SearchStack.Group>
-        </SearchStack.Navigator>
+            <Stack.Group>
+                <Stack.Screen name="SearchScreen" component={SearchScreen} />
+                <Stack.Screen name='Detail' component={DetailScreen} />
+                <Stack.Screen name='SearchDetail' component={SearchDetailScreen} />
+            </Stack.Group>
+        </Stack.Navigator>
     )
 }
+
+const FavoriteStackScreen = () => {
+    return (
+        <Stack.Navigator
+            initialRouteName='SearchScreen'
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen name='FavoriteScreen' component={FavoriteScreen} />
+            <Stack.Screen name='SearchDetail' component={SearchDetailScreen} />
+        </Stack.Navigator>
+    )
+}
+
+const HistoryStackScreen = () => {
+    return (
+        <Stack.Navigator
+            initialRouteName='SearchScreen'
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen name='HistoryScreen' component={HistoryScreen} />
+            <Stack.Screen name='SearchDetail' component={SearchDetailScreen} />
+        </Stack.Navigator>
+    )
+}
+
+
 export default function App() {
-
-
     return (
         <NavigationContainer>
             <Tab.Navigator
@@ -50,9 +75,9 @@ export default function App() {
                 initialRouteName='Search'
                 tabBar={props => <BottomNav {...props} />}
             >
-                <Tab.Screen name="History" component={HistoryScreen} />
+                <Tab.Screen name="History" component={HistoryStackScreen} />
                 <Tab.Screen name="Search" component={SearchStackScreen} />
-                <Tab.Screen name="Favorite" component={FavoriteScreen} />
+                <Tab.Screen name="Favorite" component={FavoriteStackScreen} />
             </Tab.Navigator>
         </NavigationContainer>
     );
